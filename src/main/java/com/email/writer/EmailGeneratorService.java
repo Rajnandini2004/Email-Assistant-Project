ffd7d2fc7d16f0dd3ab053da590a73aa8a3d0582
Rajnandini2004 <nandinikushwah991@gmail.com>
2025-12-16 22:13:40 +0530

    Initial Spring AI project

---
+++ src/main/java/com/email/writer/EmailGeneratorService.java
@@ -0,0 +1,51 @@
+package com.email.writer;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailGeneratorService {
+
+    private final WebClient webClient;
+    private final String apiKey;
+
+    public EmailGeneratorService(WebClient.Builder webClientBuilder,
+                                 @Value("${gemini.api.url}") String baseUrl,
+                                 @Value("${gemini.api.key}") String geminiApiKey) {
+        this.apiKey = geminiApiKey;
+        this.webClient = webClientBuilder.baseUrl(baseUrl)
+                .build();
+    }
+
+    public String generateEmailReply(EmailRequest emailRequest){
+        //Build Prompt
+        String prompt = buildPrompt(emailRequest);
+        //Prepare raw JSON Body
+        String requestBody = String.format("""
+                {
+                    "contents": [
+                      {
+                        "parts": [
+                          {
+                            "text": "%s"
+                          }
+                        ]
+                      }
+                    ]
+                  }
+                  """, prompt);
+        // Send Request
+        // Extract Response
+    }
+    private String buildPrompt(EmailRequest emailRequest){
+        StringBuilder prompt = new StringBuilder();
+        prompt.append("Generate a professional email reply for thr following email:");
+        if (emailRequest.getTone() != null && !emailRequest.getTone().isEmpty()){
+            prompt.append("Use a ").append(emailRequest.getTone()).append(" tone.");
+            // Use a casual tone
+        }
+        prompt.append("Original Email: \n").append(emailRequest.getEmailContent());
+        return prompt.toString();
+    }
+}
