d574885baa1f97707d368a0256b956da6480588e
Rajnandini2004 <nandinikushwah991@gmail.com>
2025-12-20 22:31:06 +0530

    fix: removed wrong annotation blocking my request

--- src/main/java/com/email/writer/controller/EmailGeneratorController.java
+++ src/main/java/com/email/writer/controller/EmailGeneratorController.java
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/email")
-@AllArgsConstructor
 public class EmailGeneratorController {
     private final EmailGeneratorService emailGeneratorService;
 
