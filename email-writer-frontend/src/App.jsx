0a5408590a5b139b9012e738b4927a359b8a7899
Rajnandini2004 <nandinikushwah991@gmail.com>
2025-12-22 22:50:50 +0530

    Work on frontend

--- email-writer-frontend/src/App.jsx
+++ email-writer-frontend/src/App.jsx
@@ -1,34 +1,92 @@
+import axios from 'axios';
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { Box, Button, CircularProgress, Container, FormControl, InputLabel, MenuItem, Select, TextField, Typography } from '@mui/material'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const[emailContent, setEmailContent] = useState('');
+  const[tone, setTone] = useState('');
+  const[generateReply, setGenerateReply] = useState('');
+  const[loading, setLoading] = useState(false);
+
+
+  const handleSubmit =  async () => {
+      setLoading (true);
+      try {
+        const response = await axios.post("http://localhost:8080/api/email/generate", {
+          emailContent,
+          tone
+        });
+        setGeneratedReply(typeof response.data === 'string' ?
+          response.data : JSON.stringify(response.data)
+        );
+      } catch (error) {
+        
+      }finally {
+        setLoading(false);
+      }
+   };
+
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <Container maxWidth='md' sx={{py:4}}>
+      <Typography variant='h3' component='h1' gutterBottom>
+        Email Reply Generator 
+      </Typography>
+
+      <Box sx={{mx: 3}}>
+        <TextField
+        fullWidth
+        multiline
+        rows={6}
+        variant='outlined'
+        label="Original Email Content"
+        value={emailContent || ''}
+        onChange={(e) => setEmailContent(e.target.value)}
+        sx={{mb:2}}
+        />
+        <FormControl fullWidth sx={{mb:2}}>
+            <InputLabel>Tone (Optional)</InputLabel>
+            <Select
+              value={tone || ''}
+              label="Tone (Optional)"
+              onChange={(e) => setTone(e.target.value)}
+            >
+              <MenuItem value="">None</MenuItem>
+              <MenuItem value="professional">Professional</MenuItem>
+              <MenuItem value="casual">Casual</MenuItem>
+              <MenuItem value="friendly">Friendly</MenuItem>
+            </Select>
+        </FormControl>
+        <Button variant="contained"
+        sx= {{ mb:2 }}
+        onClick={handleSubmit}
+         disabled= {!emailContent || loading}>
+          {loading ? <CircularProgress size={24} /> : 'Generate Reply'}
+      </Button>
+      </Box>
+
+      <Box sx={{mx: 3}}>
+        <TextField
+        fullWidth
+        multiline
+        rows={6}
+        variant='outlined'
+        value={generateReply || ''}
+        inputProps={{readonly: true}}
+        sx={{mb:2}}
+        />
+
+        <Button 
+        variant='outlined'
+        onClick={() => navigator.clipboard.write(generateReply)}>
+          Copy to Clipboard
+        </Button>
+      </Box>
+      
+
+    </Container>
+
   )
 }
 
